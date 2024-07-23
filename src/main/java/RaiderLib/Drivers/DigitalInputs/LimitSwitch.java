bd2ebbbf33626dbf3e1e8f30bf08be0aada99265
ChineseChen <jiachen.jiang2016@gmail.com>
2024-07-23 19:35:45 -0400

    lim switch

--- src/main/java/RaiderLib/Drivers/DigitalInputs/LimitSwitch.java
+++ src/main/java/RaiderLib/Drivers/DigitalInputs/LimitSwitch.java
@@ -1,5 +1,15 @@
 package RaiderLib.Drivers.DigitalInputs;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+
 public abstract class LimitSwitch {
+  private DigitalInput m_limitSwitch;
   
+  public LimitSwitch(int port) {
+    m_limitSwitch = new DigitalInput(port);
+  }
+
+  public boolean get() {
+    return m_limitSwitch.get();
+  }
 }
