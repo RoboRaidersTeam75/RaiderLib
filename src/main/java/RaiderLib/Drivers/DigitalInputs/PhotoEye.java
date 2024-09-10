c918f793b02e505208badf4616bbbbb0179cc94e
Daniel Lee <hexa.bricks@gmail.com>
2024-09-10 18:29:53 -0400

    photoeye

--- src/main/java/RaiderLib/Drivers/DigitalInputs/PhotoEye.java
+++ src/main/java/RaiderLib/Drivers/DigitalInputs/PhotoEye.java
@@ -1,3 +1,15 @@
 package RaiderLib.Drivers.DigitalInputs;
 
-public abstract class PhotoEye {}
+import edu.wpi.first.wpilibj.DigitalInput;
+
+public abstract class PhotoEye {
+    private DigitalInput m_photoEye;
+
+    public PhotoEye(int port) {
+        m_photoEye = new DigitalInput(port);
+    }
+
+    public boolean get() {
+        return m_photoEye.get();
+    }
+}
