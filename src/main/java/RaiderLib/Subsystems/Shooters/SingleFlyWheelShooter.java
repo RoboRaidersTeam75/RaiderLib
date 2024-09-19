9fd1bf7980f0397db7c0963092e8650cd801920b
ChineseChen <jiachen.jiang2016@gmail.com>
2024-09-19 18:59:07 -0400

    turret in progress

--- src/main/java/RaiderLib/Subsystems/Shooters/SingleFlyWheelShooter.java
+++ src/main/java/RaiderLib/Subsystems/Shooters/SingleFlyWheelShooter.java
@@ -4,10 +4,11 @@ import RaiderLib.Config.MotorConfiguration;
 import RaiderLib.Drivers.Motors.Motor;
 import RaiderLib.Drivers.Motors.Motor.MotorType;
 import RaiderLib.Drivers.Motors.MotorFactory;
+import RaiderLib.Logging.Logger;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public abstract class SingleFlyWheelShooter extends SubsystemBase {
-  private final Motor m_Flywheel;
+  private Motor m_wheel;
 
   /**
    * PIDS need to be set for this subsystem to work TODO: throw an error if pids are not configured
@@ -17,19 +18,27 @@ public abstract class SingleFlyWheelShooter extends SubsystemBase {
    * @param config the config object for the motor
    */
   public SingleFlyWheelShooter(MotorType type, MotorConfiguration config) {
-    m_Flywheel = MotorFactory.createMotor(type, config);
+    m_wheel = MotorFactory.createMotor(type, config);
   }
 
-  public void setRPM(double RPM) {
-    m_Flywheel.setRPM(RPM);
+  public void setRPM(double rpm) {
+    m_wheel.setRPM(rpm);
+  }
+
+  public void setPercent(double percent) {
+    m_wheel.setPercentOut(percent);
+  }
+
+  public void setVoltage(double voltage) {
+    m_wheel.setVoltage(voltage);
   }
 
   public double getRPM() {
-    return m_Flywheel.getRPM();
+    return m_wheel.getRPM();
   }
 
   @Override
   public void periodic() {
-    // TODO: logger addition
+    Logger.log("Single flywheel shooter speed", getRPM());
   }
 }
